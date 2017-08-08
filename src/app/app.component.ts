7982da68c310fda26db22ea89effd74bce40faa5
Kevin Charles <mrbrain@Kevins-iMac.local>
2017-08-08 11:37:40 -0400

    Jumbotron ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+   		
+
+
 }
